737af4d83aaa710ba7ace495aa01c79236270ee6
mtzsanad <mtz.sanad@gmail.com>
2022-02-10 16:17:25 +0200

    adding header

---
+++ src/components/header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+
+//Header dumy component to render application navigation
+const Header = () => {
+  return (
+    <nav className="header">
+      <header>Shopping App</header>
+    </nav>
+  );
+};
+
+export default Header;
